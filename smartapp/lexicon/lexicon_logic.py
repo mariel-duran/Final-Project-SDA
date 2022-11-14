5c291c430c865bc583c0d8f36bf45f6e1fa0aeb4
Mariel Duran <marielduran84@gmail.com>
2022-11-14 08:30:53 +0200

    Update lexicon_logic.py

--- smartapp/lexicon/lexicon_logic.py
+++ smartapp/lexicon/lexicon_logic.py
@@ -1,9 +1,17 @@
-import string
+import string, nltk
+from nltk.corpus import stopwords
 
-text_example = 'my name is mariel. I live in Estonia! Im from Dominican Republic'
+import nltk
+nltk.download('stopwords')
+
+text_example = '''
+my name is mariel. 
+I live in Estonia! 
+Im from Dominican Republic
+'''
 
 
-def remove_punctuations(text:str):
+def remove_punctuations(text: str):
     punctuation = string.punctuation
     new_text = [letter for letter in text]
     for i in range(0, len(new_text)):
@@ -15,12 +23,54 @@ def remove_punctuations(text:str):
     return modified_text
 
 
-def upper_case(text:str):
+def upper_case(text: str):
     new_text = text.upper()
     return new_text
 
 
-def lower_case(text:str):
+def lower_case(text: str):
     new_text = text.lower()
     return new_text
 
+
+def new_line_remove(text: str):
+    return "".join(text.splitlines())
+
+
+def extra_space_remove(text: str):
+    new_text = [letter for letter in text]
+    for i in range(0, len(new_text)):
+        if new_text[i] == ' ':
+            new_text[i] = ''
+        else:
+            continue
+    text_without_space = "".join(new_text)
+    return text_without_space
+
+
+def count_characters(text: str):
+    new_text = [letter for letter in text]
+    counter = 0
+    for i in range(0, len(new_text)):
+        print(new_text[i])
+        if new_text[i] == ' ':
+            continue
+        else:
+            counter += 1
+    return counter
+
+def spell_check(text: str):
+    pass
+
+
+def remove_stop_words(text: str):
+    new_text = [letter for letter in text]
+    for i in range(0, len(new_text)-1):
+        if new_text[i] in stopwords.words('english'):
+            new_text.pop(i)
+        else:
+            continue
+    text_without_stopwords = "".join(new_text)
+    return text_without_stopwords
+
+print(remove_stop_words(text_example))
